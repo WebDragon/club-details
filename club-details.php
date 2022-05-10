5fa286b131e7af587f292d7f8c5a361a91e1bc27
Scott R. Godin <github@webdragon.net>
2022-05-10 14:31:50 -0400

    First Commit

---
+++ club-details.php
@@ -0,0 +1,172 @@
+<?php 
+/*  Plugin Name: Club Details custom plugin for EFMLS
+	Description: Custom post type and editing for club membership information (location, members, dues, etc), officers, and insurance info
+	Author: Scott R. Godin for MAD House Graphics
+	Author URI: https://madhousegraphics.com
+	Version: 0.9
+	License: GPL3
+ */
+
+// NOTE	import/export/bulk-edit logic can be handled by https://www.wpallimport.com/export-advanced-custom-fields/
+//		and does not need to be integrated into the plugin
+//
+// TODO	add check to make sure ACF Pro is enabled 
+// TODO	add display logic in meta-box with dues/liability calculations based on member count
+// TODO	add front-end display logic for the clubdetails Loop and for the single-posts 
+//		[may not be internally required as it seems possible to do this within Elementor itself using Elementor Custom Templates]
+
+date_default_timezone_set("UTC");
+setlocale( LC_MONETARY, 'en_US.UTF-8');
+
+define( 'CLUB_ROOT',	plugins_url( '', __FILE__ ) );
+define( 'CLUB_ASSETS',	plugin_dir_path( __FILE__ ) . 'assets/' );
+//define( 'CLUB_IMAGES',	CLUB_ASSETS . 'images/' );
+define( 'CLUB_STYLES',	CLUB_ASSETS . 'css/' );
+define( 'CLUB_SCRIPTS',	CLUB_ASSETS . 'js/' );
+//define( 'CLUB_INCL',	CLUB_ASSETS . 'inc/' );
+
+// {{{ Register Custom Post Type for EFMLS Club Details 
+function register_efmls_club() {
+
+	$clublabels = array(
+		'add_new_item'			=> __("Add Club Details", 'efmls'),
+		'all_items'				=> __("All Clubs", 'efmls'),
+		'archives'				=> __("Club Archives", 'efmls'),
+		'attributes'			=> __("Club attributes", 'efmls'),
+		'edit_item'				=> __("Edit Club Details", 'efmls'),
+		'featured_image'		=> __("Featured Image for this Club", 'efmls'),
+		'filter_items_list'		=> __("Filter Club List", 'efmls'),
+		'items_list'			=> __("Club list", 'efmls'),
+		'items_list_navigation'	=> __("Club list navigation", 'efmls'),
+		'menu_name'				=> __("Club Details", 'efmls'),
+		'name'					=> __("Club Details", 'efmls'),
+		'new_item'				=> __("New Club", 'efmls'),
+		'not_found'				=> __("No Clubs found", 'efmls'),
+		'not_found_in_trash'	=> __("No Clubs found in Trash", 'efmls'),
+		'remove_featured_image'	=> __("Remove Featured Club image", 'efmls'),
+		'search_items'			=> __("Search Clubs", 'efmls'),
+		'set_featured_image'	=> __("Set Featured Club image", 'efmls'),
+		'singular_name'			=> __("Club Details", 'efmls'),
+		'use_featured_image'	=> __("Use as Featured Image for this Club", 'efmls'),
+		'view_item'				=> __("View Club", 'efmls'),
+		'view_items'			=> __("View Clubs", 'efmls'),
+	);
+	$clubsupports =  array(
+		'author',
+		'custom-fields',
+		'editor',
+		'page-attributes',
+		'revisions',
+		'thumbnail',
+		'title',
+	);
+	$clubargs = array(
+		'can_export'			=> true,
+		'capability_type'		=> "post",
+		'description'			=> "EFMLS Societies/Clubs featured on the website",
+		'has_archive'			=> true,
+		'hierarchical'			=> false,
+		'labels'				=> $clublabels,
+		'map_meta_cap'			=> true,
+		'menu_icon'				=> "dashicons-hammer",
+		'menu_position'			=> 10,
+		'public'				=> true,
+		'publicly_queryable'	=> true,
+		'query_var'				=> true,
+		'rest_base'				=> "",
+		'rewrite'				=> array('slug' => 'clubdetails', 'with_front' => false),
+		'show_in_menu'			=> true,
+		'show_in_rest'			=> false,
+		'show_ui'				=> true,
+		'supports'				=> $clubsupports,
+		'taxonomies'			=> array('post_tag'),
+		'register_meta_box_cb'	=> 'rate_calculation_meta_box',
+	);
+	register_post_type('clubdetails', $clubargs );
+
+    $clubregions = array(
+        'labels' => array(
+			'name' => 'Regions',
+			'singular_item' => 'Region',
+			'add_new_item' => 'Add New Region',
+			'edit_item' => 'Edit Region',
+        ),
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => false,
+        'show_in_rest' => false,
+        'show_tagcloud' => false,
+    );
+    register_taxonomy( 'clubregions', array( 'clubdetails' ), $clubregions );
+
+}
+add_action('init', 'register_efmls_club'); // }}}
+
+// {{{ Flushing rewrite rules on plugin activation/deactivation for better working permalink structure
+function club_activation_deactivation() {
+
+	register_efmls_club();
+	flush_rewrite_rules();
+
+} 
+register_activation_hook( __FILE__, 'club_activation_deactivation' ); // }}}
+
+// {{{ Register custom Settings page for dues and insurance multipliers with ACF Pro
+if ( function_exists('acf_add_options_page') ) {
+
+	$settings_icon = efmls_inline_dashicon('admin-settings');
+
+	acf_add_options_page( array( 
+		'page_title'	=> "Dues and Insurance Rates",
+		'menu_title'	=> "Dues and Insurance Rates {$settings_icon}", // Fine, icon shoehorned in because the below icon does not display
+		'menu_slug'		=> 'rate-settings',
+		'parent_slug'	=> 'edit.php?post_type=clubdetails',
+		'capability'	=> 'manage_options',
+		'icon_url'		=> 'dashicons-admin-settings', // why does the admin submenu not display this icon?
+		'position'		=> false,
+	));
+
+} // }}}
+
+// {{{ Add Meta Box for display of Dues & Insurance calculations
+// included by callback in custom post type creation args above
+function rate_calculation_meta_box() {
+
+	add_meta_box(
+		'rate-calculation',
+		__("Dues & Insurance Rate Calculation", 'efmls'),
+		'rate_calculation_callback',
+		'clubdetails',
+		'side', 'high', null
+	);
+
+}
+
+// Meta Box Content
+function rate_calculation_callback( $post ) {
+
+	wp_nonce_field('rate_calculation_nonce','rate_calculation_nonce');
+
+	// TODO : Optional - populate the data values live as numbers are entered in the respective fields for the custom post type member counts
+	// TODO : add test to ensure options fields have been populated with rate calculation values that are valid. 
+	echo <<<HTML
+	<p><b>Total Members</b>: <!-- add numbers from ACF fields for junior and senior members and insert value here --> <br>
+		<small><i>includes Seniors + Juniors</i></small></p>
+	<p><b>Dues</b>:<!-- use total members in calculation, multiplied by rate value for dues from ACF Options page for this CPT --></p>
+	<p><b>Insurance</b>:<!-- use total members in calculation, multiplied by rate value for insurance from ACF Options page for this CPT --></p>
+
+HTML;
+
+}
+// }}}
+
+function efmls_inline_dashicon( $icon_type ) {
+	return <<<HTML
+		<span class="wp-menu-image dashicons-before dashicons-{$icon_type}" aria-hidden="true"></span>
+HTML;
+}
+
+/* vim600: set ft=php.html ts=4 sw=4 noexpandtab foldmethod=marker : */
+?>
